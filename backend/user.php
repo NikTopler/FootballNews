f57b3cd655e4a885c1fff02691c6bd05f2372ea6
Nik Topler <nik.topler21@gmail.com>
2021-03-23 21:04:24 +0100

    Create user exist check function

--- backend/user.php
+++ backend/user.php
@@ -3,4 +3,12 @@ include_once 'db.php';
 
 class User extends Dbh {
 
+    public function checkIfUserExists($email) {
+        $sql = 'SELECT * FROM users WHERE email = ?';
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$email]);
+        $row = $stmt->fetch();
+        if($row) echo 'User exists';
+        else echo 'User doesn\'t exist';
+    }
 }
