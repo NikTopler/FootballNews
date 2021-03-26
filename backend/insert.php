ddf39abe02fcbf32a0d41b025341453ecaae29cb
Nik Topler <nik.topler21@gmail.com>
2021-03-26 21:37:39 +0100

    Basic setup for inserting data into the db

--- backend/insert.php
+++ backend/insert.php
@@ -3,6 +3,19 @@ include_once 'user.php';
 
 class Signin extends User {
 
+  public function standardSignup($userInfo) {
+    for($i = 0; $i < count($userInfo); $i++) {
+        echo $userInfo[$i];
+    }
+  }
+
+  public function checkIfUserExists($email) {
+      $sql = 'SELECT * FROM users WHERE email = ?';
+      $stmt = $this->connect()->prepare($sql);
+      $stmt->execute([$email]);
+      $row = $stmt->fetch();
+  }
 }
 
 $signinObj = new Signin();
+if(isset($_POST['standard'])) $signinObj->standardSignup(json_decode($_POST['standard']));
