25f75703e8a3ae8dc11b7c06f40ab1ceefbc649a
Nik Topler <nik.topler21@gmail.com>
2021-03-27 12:02:29 +0100

    Verification
    Add function for double-checking if user values are correct

--- backend/insert.php
+++ backend/insert.php
@@ -14,6 +14,18 @@ class Signin extends User {
       $stmt = $this->connect()->prepare($sql);
       $stmt->execute([$email]);
       $row = $stmt->fetch();
+  public function errorHandeling($userInfo) {
+    $string = '';
+    if(empty($userInfo[0])) $string = $string.' name';
+    if(empty($userInfo[1])) $string = $string.' surname';
+    if(empty($userInfo[2])) $string = $string.' email';
+    if(empty($userInfo[3])) $string = $string.' password';
+    if(empty($userInfo[4])) $string = $string.' password-repeat';
+    if($string != '') $this->errorOver($string);
+    if(!filter_var($userInfo[2], FILTER_VALIDATE_EMAIL)) $this->errorOver('incorrect email');
+    if(preg_match('/\s/', $userInfo[3])) $this->errorOver("no white spaces in password");
+  }
+
   public function errorOver($string) {
     echo $string;
     die;
