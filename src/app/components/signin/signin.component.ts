247ccecbb3c9e899a5f81af780512db1d4c1f3e0
Nik Topler <nik.topler21@gmail.com>
2021-03-27 15:31:16 +0100

    Update signin.component.ts

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CommService } from 'src/app/services/comm/comm.service';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-signin',
@@ -14,13 +15,20 @@ export class SigninComponent {
   constructor(
     private fb: FormBuilder,
     private comm: CommService
-  ) {
+  ){
     this.signinForm = this.fb.group({
-
+      email: ['', [Validators.required, Validators.email]],
+      psw: ['', [Validators.required]]
     });
   }
 
-  onSubmit() {
+  async onSubmit() {
+    if(!this.signinForm.valid) return;
 
+    const userInfo = JSON.stringify(Object.values(this.signinForm.value));
+    const req = await fetch(`${environment.db}/check.php`, {
+      method: 'POST',
+      body: this.comm.createFormData('signin', userInfo)
+    })
   }
 }
