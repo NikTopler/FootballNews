459d96526f90cc5aef6a53fe72fc30531d424e9d
Nik Topler <nik.topler21@gmail.com>
2021-03-31 23:13:37 +0200

    Move social login to app.component.ts

--- src/app/pages/authentication/external/external.component.ts
+++ src/app/pages/authentication/external/external.component.ts
@@ -1,55 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { SocialAuthService } from 'angularx-social-login';
-import { environment } from '../../../../environments/environment';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
-import { CommService } from 'src/app/services/comm/comm.service';
 
 @Component({
   selector: 'app-external',
   templateUrl: './external.component.html',
   styleUrls: ['./external.component.scss']
 })
-export class ExternalComponent implements OnInit {
+export class ExternalComponent {
 
-  userInfo: any;
-  loggedIn: boolean = false;
+  constructor(private authentication: AuthenticationService) { }
 
-  constructor(
-    private authentication: AuthenticationService,
-    private socialAuthService: SocialAuthService,
-    private comm: CommService) { }
-
-  ngOnInit(): void {
-    this.socialAuthService.authState.subscribe((user) => {
-      this.userInfo = user.provider !== 'AMAZON' ? user : {
-        "id": user.id,
-        "firstName": user.name.split(' ')[0],
-        "lastName": user.name.split(' ')[1],
-        "email": user.email,
-        "photoUrl": null,
-        "provider": 'AMAZON'
-      }
-      this.loggedIn = (user != null);
-      if(this.loggedIn) this.socialLogin(this.userInfo);
-    });
-  }
+  @Output() loginPopup = new EventEmitter<boolean>();
+  @Output() social = new EventEmitter();
 
   google(): void { this.authentication.loginWithGoogle(); }
   facebook(): void { this.authentication.loginWithFacebook(); }
   amazon(): void { this.authentication.loginWithAmazon(); }
-  logout(): void { this.authentication.logOut(); }
 
-  async socialLogin(
-    {id, firstName, lastName, email ,photoUrl, provider} :
-    {id: string, firstName: string, lastName: string, email: string,photoUrl: string, provider: string}
-  ) {
-    const userInfo = JSON.stringify(Object.values({id, firstName, lastName, email, photoUrl, provider}));
-    const req = await fetch(`${environment.db}/insert.php`, {
-      method: 'POST',
-      body: this.comm.createFormData('SOCIAL', userInfo)
-    });
-    const res = await req.text();
-    console.log(res);
-  }
-
+  closeLogin(val: boolean) { this.loginPopup.emit(val); }
 }
