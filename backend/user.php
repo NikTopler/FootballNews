eb6e324412acc7c20c6db23d3ea451cae0d5aed8
Nik Topler <nik.topler21@gmail.com>
2021-04-04 17:08:48 +0200

    Token authentication 1.2

--- backend/user.php
+++ backend/user.php
@@ -1,5 +1,7 @@
 <?php
-include_once 'db.php';
+include_once 'config/db.php';
+require __DIR__ . '/libs/vendor/autoload.php';
+use \Firebase\JWT\JWT;
 
 $allowed_domains = ["http://localhost:4200", "https://footballnews-app.herokuapp.com"];
 
@@ -27,43 +29,14 @@ class User extends Dbh {
     else return 2;
   }
 
-  public function checkSession() {
-    if(session_id() == '') $this->message('No session');
-    else $this->returnUserData();
-  }
-
-  public function getSession($email) {
-    if(session_id() == '') session_start();
 
     $sql = 'SELECT * FROM users WHERE email = ?';
     $stmt = $this->connect()->prepare($sql);
     $stmt->execute([$email]);
     $row = $stmt->fetch();
 
-    $_SESSION['id'] = $row["id"];
-    $_SESSION['firstName'] = $row["firstName"];
-    $_SESSION['lastName'] = $row["lastName"];
-    $_SESSION['email'] = $row["email"];
-    $_SESSION['admin'] = $row["admin"];
-    $_SESSION['profileImg'] = $row["profileImg"];
-    $_SESSION['googleID'] = $row["googleID"];
-    $_SESSION['facebookID'] = $row["facebookID"];
-    $_SESSION['amazonID'] = $row["amazonID"];
   }
 
-  public function returnUserData() {
-    echo '{
-      "id": "'.$_SESSION["id"].'",
-      "firstName": "'.$_SESSION["firstName"].'",
-      "lastName": "'.$_SESSION["lastName"].'",
-      "email": "'.$_SESSION["email"].'",
-      "admin": '.$_SESSION["admin"].',
-      "profileImg": "'.$_SESSION["profileImg"].'",
-      "googleID": "'.$_SESSION["googleID"].'",
-      "facebookID": "'.$_SESSION["facebookID"].'",
-      "amazonID": "'.$_SESSION["amazonID"].'"
-    }';
-    die;
   }
 
   public function message($string) {
