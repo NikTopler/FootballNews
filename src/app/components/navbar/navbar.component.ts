7decfa1e8f3895fc7aa090c20d1a88e40431d5d0
Nik Topler <nik.topler21@gmail.com>
2021-04-04 19:57:03 +0200

    Update navbar functionality

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private authenticationService: AuthenticationService) { }
+
+  ngOnInit() { }
+
+  @Input() userInfo: any = null;
+  @Input() loggedIn: boolean = false;
 
   @Output() loginPopup = new EventEmitter<boolean>();
   openLogin(val: boolean) { this.loginPopup.emit(val); }
+
+  logout() { this.authenticationService.logout(); }
 
 }
 
