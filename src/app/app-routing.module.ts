10a19f3dac2c0069079cf5f14c47d299c8daa2da
Nik Topler <nik.topler21@gmail.com>
2021-04-05 09:59:27 +0200

    Setup routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './pages/admin/admin.component';
 import { HomeComponent } from './pages/home/home.component';
-import { SigninComponent } from './pages/authentication/signin/signin.component';
-import { SignupComponent } from './pages/authentication/signup/signup.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { AccountComponent } from './components/account/account.component';
+import { UserGuard } from './guards/user/user.guard';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
   { path: 'admin', component: AdminComponent },
-  { path: 'signin', component: SigninComponent },
-  { path: 'signup', component: SignupComponent }
+  { path: 'settings', pathMatch: 'full', redirectTo: 'settings/account' },
+  { path: 'settings', component: SettingsComponent },
+  { path: 'settings/account', component: AccountComponent }
 ];
 
 @NgModule({
