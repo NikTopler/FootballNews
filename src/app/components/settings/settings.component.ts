27ccbc36910ab1432d46017d2a403312145f255c
Nik Topler <nik.topler21@gmail.com>
2021-04-06 11:10:56 +0200

    Add basic page layout

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -8,8 +8,19 @@ import { Router } from '@angular/router';
 })
 export class SettingsComponent implements OnInit {
 
+  accountSettingsSection: Array<any> = [
+    { name: 'Account', path: 'account', active: this.router.url.includes('account') ? 'active' : ''  },
+    { name: 'Update account', path: 'update', active: this.router.url.includes('update') ? 'active' : '' },
+    { name: 'Email', path: 'email', active: this.router.url.includes('email') ? 'active' : ''}
+  ];
+
+  preferencesSection: Array<any> = [
+    { name: 'Theme', path: 'theme', active: this.router.url.includes('theme') ? 'active' : ''  },
+  ]
+
   constructor(private router: Router) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+  }
 
 }
