e504ece6956711f6f5474ccacef22898ca3e953d
Nik Topler <nik.topler21@gmail.com>
2021-04-07 20:32:09 +0200

    Code cleanup

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AppComponent } from 'src/app/app.component';
 import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
