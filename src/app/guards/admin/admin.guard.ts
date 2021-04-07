337d8a470113b56c06b14eb91a6d4836225befa9
Nik Topler <nik.topler21@gmail.com>
2021-04-07 14:25:15 +0200

    Setup admin guard

--- src/app/guards/admin/admin.guard.ts
+++ src/app/guards/admin/admin.guard.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AppComponent } from 'src/app/app.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AdminGuard implements CanActivate {
+
+  constructor(private app: AppComponent) { }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+    return this.app.checkAuthentication()
+      .then(() => {return this.app.userInfo.admin == 1 ? true : false})
   }
-  
+
 }
