da3e4a47e75950b453a72ba9c52d2ce80b39ef41
Nik Topler <nik.topler21@gmail.com>
2021-04-13 18:05:28 +0200

    User body validation

--- src/app/services/import-verification/import-verification.service.ts
+++ src/app/services/import-verification/import-verification.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
+import { AuthenticationService } from '../authentication/authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ImportVerificationService {
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService) { }
+
   userFormStructure = [
     ['first name', 'firstname'],
     ['last name', 'lastname', 'surname'],
@@ -41,4 +43,17 @@ export class ImportVerificationService {
     return errArray;
   }
 
+  userValidation = (word: string, i: number, j: number, errArray: string[]) => {
+    if((j === 0 || j === 1) && !this.authenticationService.firstLastName(word))
+      errArray.push(i + '-' + j);
+    else if(j === 2 && !this.authenticationService.validateEmail(word))
+      errArray.push(i + '-' + j);
+    else if(j === 3 &&
+        (word.toLowerCase() !== 'true'
+      && word.toLowerCase() !== 'false') &&
+        (word !== '0' && word !== '1'))
+      errArray.push(i + '-' + j);
+
+    return errArray;
+  }
 }
