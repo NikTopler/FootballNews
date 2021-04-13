0335d744c96196c14f5ddd47309d9e1a943c1784
Nik Topler <nik.topler21@gmail.com>
2021-04-13 18:04:03 +0200

    Loop through double array and validate based on type

--- src/app/services/import-verification/import-verification.service.ts
+++ src/app/services/import-verification/import-verification.service.ts
@@ -7,4 +7,18 @@ export class ImportVerificationService {
 
   constructor() { }
 
+  importValidation(type: string, func: any, array: string[][]) {
+    let errArray: string[] = [];
+    for(let i = 0; i < array.length; i++) {
+      for(let j = 0; j < array[i].length; j++) {
+        if(i === 0) {
+          if(!this.headerImport(type, array[i][j]))
+            errArray.push(i + '-' + j);
+        }
+        else errArray = func(array[i][j], i, j, errArray);
+      }
+    }
+    return errArray;
+  }
+
 }
