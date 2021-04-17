33059e9fd63a61defa473e787b2c425df049f06c
Nik Topler <nik.topler21@gmail.com>
2021-04-17 12:21:33 +0200

    Update guards

--- src/app/guards/admin/admin.guard.ts
+++ src/app/guards/admin/admin.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AppComponent } from 'src/app/app.component';
 
@@ -8,14 +8,16 @@ import { AppComponent } from 'src/app/app.component';
 })
 export class AdminGuard implements CanActivate {
 
-  constructor(private app: AppComponent) { }
+  constructor(
+    private app: AppComponent,
+    private router: Router) { }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
     return this.app.checkAuthentication()
-      .then(() => {return this.app.userInfo.admin == 1 ? true : false})
+      .then(() => { return this.app.userInfo.admin == 1 ? true : this.router.parseUrl('/home'); })
   }
 
 }
--- src/app/guards/user/user.guard.ts
+++ src/app/guards/user/user.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AppComponent } from 'src/app/app.component';
 
@@ -8,14 +8,16 @@ import { AppComponent } from 'src/app/app.component';
 })
 export class UserGuard implements CanActivate {
 
-  constructor(private app: AppComponent) { }
+  constructor(
+    private app: AppComponent,
+    private router: Router) { }
 
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
     return this.app.checkAuthentication()
-    .then((res) => {return this.app.loggedIn ? true : false})
+    .then(() => {return this.app.loggedIn ? true : this.router.parseUrl('/home')})
   }
 
 }
