e9d5e95bec95e1492908fb380208d3b386031c70
Nik Topler <nik.topler21@gmail.com>
2021-04-19 19:23:48 +0200

    Refactor

--- src/app/components/settings/admin/insight/imports/imports.component.ts
+++ src/app/components/settings/admin/insight/imports/imports.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Chart, ChartType, ChartOptions } from 'chart.js';
+import { ChartType, ChartOptions } from 'chart.js';
 import { SingleDataSet, Label, Color } from 'ng2-charts';
 import { CommService } from 'src/app/services/comm/comm.service';
 import { UserService } from 'src/app/services/user/user.service';
@@ -23,7 +23,7 @@ export class ImportsComponent {
   lineChartLabels: Label[] = [];
   lineChartData: SingleDataSet = [];
   lineChartType: ChartType = 'line';
-  lineChartLegend = true;
+  lineChartLegend = false;
   lineChartPlugins = [];
 
   colors: Color[] = [
@@ -35,7 +35,6 @@ export class ImportsComponent {
       ]
     }
   ];
-
   constructor(
     private comm: CommService,
     private userService: UserService) { this.getValues(); }
