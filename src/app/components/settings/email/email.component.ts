ad7f663c4a8731034f7cfbbdfcc01551e381413d
Nik Topler <nik.topler21@gmail.com>
2021-04-20 23:07:13 +0200

    Import/declare

--- src/app/components/settings/email/email.component.ts
+++ src/app/components/settings/email/email.component.ts
@@ -1,18 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { AppComponent } from 'src/app/app.component';
+import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
+import { CommService } from 'src/app/services/comm/comm.service';
+import { UserService } from 'src/app/services/user/user.service';
+import { environment } from '../../../../environments/environment';
+import { SettingsComponent } from '../settings.component';
 
 @Component({
   selector: 'app-email',
   templateUrl: './email.component.html',
   styleUrls: ['./email.component.scss']
 })
-export class EmailComponent implements OnInit {
+export class EmailComponent {
 
   userInfo: any = this.app.userInfo;
+  subscribed: boolean = Number(this.app.userInfo.emailingService) === 0 ? false : true;
 
-  constructor(private app: AppComponent) { }
+  allLeagues: string[] = [];
+  followingLeagues: string[] = [];
 
-  ngOnInit(): void {
+  constructor(
+    private app: AppComponent,
+    private userService: UserService,
+    private comm: CommService,
+    private authenticationService: AuthenticationService,
+    private SettingsComponent: SettingsComponent,
+    private AppComponent: AppComponent) {
+      this.getAllLeagues();
+      this.getFollowList();
+    }
+
   }
 
 }
