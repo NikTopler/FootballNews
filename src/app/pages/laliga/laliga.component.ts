0a3c05425518376b90d4260451c90262dc992571
Gaber Cujes <51849848+Gaberthepro@users.noreply.github.com>
2021-04-20 18:43:18 +0200

    top in laliga and primerleague

--- src/app/pages/laliga/laliga.component.ts
+++ src/app/pages/laliga/laliga.component.ts
@@ -11,6 +11,7 @@ import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 export class LaligaComponent implements OnInit {
 
   array: any;
+  top:any;
   date1 = new FormControl(new Date())
   constructor(private http: HttpClient) { }
 
@@ -35,6 +36,12 @@ export class LaligaComponent implements OnInit {
       this.array = this.array.data;
     });
 
+
+    let a = this.http.get("https://app.sportdataapi.com/api/v1/soccer/topscorers?apikey=2f2b7820-86f4-11eb-b165-0792cfd2240a&season_id=1511");
+    a.subscribe((data) => {
+      this.top = data;
+      this.top = this.top.data.slice(0,5);
+    })
 
   }
 
