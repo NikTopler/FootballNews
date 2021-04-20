b578e2bc0c06cac62bb40c80d03c100e6e264cc3
Nik Topler <nik.topler21@gmail.com>
2021-04-20 23:08:09 +0200

    Compare and manage leagues

--- src/app/components/settings/email/email.component.ts
+++ src/app/components/settings/email/email.component.ts
@@ -88,4 +88,27 @@ export class EmailComponent {
     this.followingLeagues = JSON.parse(res).leagues;
   }
 
+  compareLeagues(league: string, array: string[]) {
+    for(let i = 0; i < array.length; i++)
+      if(league.toLowerCase() === array[i].toLowerCase())
+        return true;
+    return false;
+  }
+
+  async manageFollowLeagues(e: any, league: string) {
+    let follow: string = 'FOLLOW_LEAGUE';
+
+    if(e.target.innerHTML === 'Unfollow') follow = 'UNFOLLOW_LEAGUE';
+
+    const isUserValidated = await this.validateUser();
+    if(!isUserValidated) return location.reload();
+
+    const email = JSON.stringify({ "email": this.userInfo.email, "leagueName": league });
+    const req = await fetch(`${environment.db}/update.php`, {
+      method: 'POST',
+      body: this.comm.createFormData(follow, email)
+    });
+    const res = await req.text();
+    this.getFollowList();
+  }
 }
