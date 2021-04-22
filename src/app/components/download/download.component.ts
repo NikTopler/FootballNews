b9ce51f0eede14536f15041c883bf26cbe333b50
Nik Topler <nik.topler21@gmail.com>
2021-04-22 23:13:29 +0200

    Update download.component.ts

--- src/app/components/download/download.component.ts
+++ src/app/components/download/download.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AppComponent } from 'src/app/app.component';
+import { DownloadService } from 'src/app/services/download/download.service';
 
 @Component({
   selector: 'app-download',
   templateUrl: './download.component.html',
   styleUrls: ['./download.component.scss']
 })
-export class DownloadComponent implements OnInit {
+export class DownloadComponent {
 
-  constructor() { }
+  open: boolean = false;
+  downloadsOpen: boolean = true;
+  rotateIcon: string = '';
 
-  ngOnInit(): void {
+  public downloadsArray: downloadArray[] = [];
+
+  constructor(
+    private appComponent: AppComponent,
+    private downloadService: DownloadService) {
+    this.downloadsArray = downloadService.downloadsArray;
   }
 
+  transformArrayToXLSX(type: string, array: string[][]) { }
+
+  close(): void {
+    this.downloadService.downloadsArray = [];
+    this.appComponent.downloadOpen = false;
+  }
+}
+
+export interface downloadArray {
+  text: string,
+  finished: boolean
 }
