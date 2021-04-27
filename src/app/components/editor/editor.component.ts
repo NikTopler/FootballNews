63b7ca2d842b2e034a4ce2ba047b8014d1e8f8b5
Nik Topler <nik.topler21@gmail.com>
2021-04-27 14:44:27 +0200

    Setup for "manual changes to text"

--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EditorService } from 'src/app/services/editor/editor.service';
 
 @Component({
   selector: 'app-editor',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorComponent implements OnInit {
 
-  constructor() { }
+  isIntervalRunning: boolean = false;
+  intervalArray: any[] = [];
+  contentArray: any[] = this.editorService.activeArray;
 
-  ngOnInit(): void {
-  }
+  constructor(private editorService: EditorService) { }
 
+  async ngOnInit() { }
+
+  get getBlinkingCursorElement() { return document.querySelector('.blinking-cursor') as HTMLDivElement }
+  get getContentContainer() { return document.getElementById('content-container') as HTMLDivElement }
+  get getRowContentContainer() { return document.getElementById('row-container') as HTMLDivElement }
+  get getAllPreElements() { return document.querySelectorAll('pre'); }
 }
