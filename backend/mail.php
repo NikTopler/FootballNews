b33cb3788ec3a91be405556205f87984ade46837
Nik Topler <nik.topler21@gmail.com>
2021-05-04 17:17:13 +0200

    Make inserting more secure

--- backend/mail.php
+++ backend/mail.php
@@ -14,12 +14,26 @@ class Mail extends Admin {
 
       mail($data->emails[$i], $data->subject, $data->message, $headers);
 
-      $sql = 'INSERT INTO sendEmails(time, subject, message, email) VALUES(?, ?, ?, ?)';
-      $stmt = $this->connect()->prepare($sql);
-      $stmt->execute([$date, $data->subject, $data->message, $data->emails[$i]]);
+      try {
+        $sql = 'INSERT INTO sendEmails(time, subject, message, email) VALUES(?, ?, ?, ?)';
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute([$date, $data->subject, $data->message, $data->emails[$i]]);
+      } catch(Exception $e) {
+        echo json_encode(array(
+          "status" => 404,
+          "data" => $e
+        ));
+      }
 
       $this->adminCheckUp($data->adminEmail, $date, 'email', 'email', 'sendEmails_id', null);
     }
+
+    echo json_encode(array(
+      "status" => "ok",
+      "data" => ""
+    ));
+  }
+
   }
 }
 
