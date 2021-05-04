064c2b7688330d4b4bef816c8ea8e58dd58cb4a0
Nik Topler <nik.topler21@gmail.com>
2021-05-04 12:05:35 +0200

    Setup mailing service

--- backend/mail.php
+++ backend/mail.php
@@ -3,7 +3,27 @@ include_once 'admin.php';
 
 class Mail extends Admin {
 
+  public function customEmail($data) {
+
+    $date = date(time());
+
+    for($i = 0; $i < count($data->emails); $i++) {
+      $headers = '';
+      for($j = 0; $j < count($data->headers); $j++)
+        $headers .= $data->headers[$j]."\r\n";
+
+      mail($data->emails[$i], $data->subject, $data->message, $headers);
+
+      $sql = 'INSERT INTO sendEmails(time, subject, message, email) VALUES(?, ?, ?, ?)';
+      $stmt = $this->connect()->prepare($sql);
+      $stmt->execute([$date, $data->subject, $data->message, $data->emails[$i]]);
+
+      $this->adminCheckUp($data->adminEmail, $date, 'email', 'email', 'sendEmails_id', null);
+    }
+  }
 }
 
 $mailObj = new Mail();
 if($_SERVER['REQUEST_METHOD'] !== 'POST') die;
+if(isset($_POST['CUSTOM_EMAIL'])) $mailObj->customEmail(json_decode($_POST['CUSTOM_EMAIL']));
+
