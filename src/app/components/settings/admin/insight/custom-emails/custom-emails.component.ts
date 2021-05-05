e7604891b719df2193e9a4c6bfffb7b855272445
Nik Topler <nik.topler21@gmail.com>
2021-05-05 08:50:31 +0200

    Import services and declare variables

--- src/app/components/settings/admin/insight/custom-emails/custom-emails.component.ts
+++ src/app/components/settings/admin/insight/custom-emails/custom-emails.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartOptions, ChartType } from 'chart.js';
+import { Color, Label, SingleDataSet } from 'ng2-charts';
+import { CommService } from 'src/app/services/comm/comm.service';
+import { UserService } from 'src/app/services/user/user.service';
+import { environment } from '../../../../../../environments/environment';
 
 @Component({
   selector: 'app-custom-emails',
   templateUrl: './custom-emails.component.html',
   styleUrls: ['./custom-emails.component.scss']
 })
-export class CustomEmailsComponent implements OnInit {
+export class CustomEmailsComponent {
 
-  constructor() { }
+  pieChartOptions: ChartOptions = { responsive: true, maintainAspectRatio: false };
+  pieChartLabels: Label[] = [];
+  pieChartData: SingleDataSet = [];
+  pieChartType: ChartType = 'pie';
+  pieChartLegend = true;
+  pieChartPlugins = [];
 
-  ngOnInit(): void {
-  }
+  lineChartOptions: ChartOptions = { responsive: true, maintainAspectRatio: false };
+  lineChartLabels: Label[] = [];
+  lineChartData: SingleDataSet = [];
+  lineChartType: ChartType = 'line';
+  lineChartLegend = false;
+  lineChartPlugins = [];
+
+  colors: Color[] = [
+    {
+      backgroundColor: [
+        '#ff4081',
+        'rgba(0,172,193,1)',
+        'rgba(84,58,183,1)'
+      ]
+    }
+  ];
+
 
 }
