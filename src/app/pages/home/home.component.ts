824ca4e83961fa5810f0bf82693b562654f277e6
Nik Topler <nik.topler21@gmail.com>
2021-05-24 22:07:05 +0200

    Add functionality to latest news slider

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CommService } from 'src/app/services/comm/comm.service';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -7,12 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  latestNewsArray: any[] = [];
+  showLeftArrow: boolean = false;
+  showRightArrow: boolean = true;
+  direction: number = 0;
 
+  constructor(private comm: CommService) { this.setupLatestNews() }
 
+  ngOnInit() { }
+
+  changeLatestNews(direction: string) {
+
+    direction === 'left' ? this.direction += 285 : this.direction += -285;
+
+    if(this.direction === 0) {
+      this.showLeftArrow = false;
+      this.showRightArrow = true;
+    } else if(this.direction * (-1) === (285 * 10 - 285 * 4)) {
+      this.showLeftArrow = true;
+      this.showRightArrow = false;
+    } else {
+      this.showLeftArrow = true;
+      this.showRightArrow = true;
+    }
+
+    for(let i = 0; i < this.getAllLatestNews.length; i++) {
+      let element = this.getAllLatestNews[i] as HTMLDivElement;
+      element.style.transform = `translateX(${this.direction}px)`;
+    }
   }
 
-  }
-
+  get getAllLatestNews() { return document.querySelectorAll('.news-article'); }
 
   ngOnInit() { }
 }
