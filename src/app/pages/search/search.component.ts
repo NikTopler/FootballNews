6a3faec490170f1994e5525fa44408ccad4a1c64
Nik Topler <nik.topler21@gmail.com>
2021-05-28 00:02:49 +0200

    Setup search component

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommService } from 'src/app/services/comm/comm.service';
+import { SearchService } from 'src/app/services/search/search.service';
+import { UserService } from 'src/app/services/user/user.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
 
-  constructor() { }
+  searchArticleArray: any[] = [];
+  searchNumber: number = 0;
 
-  ngOnInit(): void {
+  allLeagues: any[] = [];
+  following: any = [];
+
+  constructor(
+    private router: Router,
+    private comm: CommService,
+    private userService: UserService,
+    private searchService: SearchService
+    ) {
+      searchService.getsearchArticleArray()
+        .subscribe((data) => { this.searchArticleArray = data; })
+      userService.getUserData()
+        .subscribe((userInfo) => { this.following = userInfo.following; })
+      comm.setIsLoaded(true);
+      this.getAllLeagues();
+    }
+
+  async getAllLeagues() { this.allLeagues = await this.comm.getAllLeagues(); }
+
+  addActiveClass(name: string) {
+    if(!this.following) return '';
+    for(let i = 0; i < this.following.length; i++)
+      if(this.following[i].name === name)
+        return 'active';
+    return '';
   }
 
+  openLink(url: string) { window.open(url); }
+  openPage(page: string) { this.router.navigateByUrl(page); }
 }
