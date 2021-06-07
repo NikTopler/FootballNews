338469bfdd90e9e855dd7a09fb54542952bbf556
Nik Topler <nik.topler21@gmail.com>
2021-06-07 09:51:12 +0200

    Declare variables/observables

--- src/app/services/league/league.service.ts
+++ src/app/services/league/league.service.ts
@@ -1,16 +1,32 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
+import { environment} from '../../../environments/environment';
+import { CommService } from '../comm/comm.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LeagueService {
 
-  $openLeague: BehaviorSubject<string>;
+  openLeague: string = '';
 
-  constructor() { this.$openLeague = new BehaviorSubject<string>(''); }
+  $openLeague: BehaviorSubject<string>;
+  $players: BehaviorSubject<any[]>;
+  $allTeams: BehaviorSubject<any[]>;
+
+  constructor(private comm: CommService) {
+    this.$openLeague = new BehaviorSubject<string>('');
+    this.$players = new BehaviorSubject<any[]>([]);
+    this.$allTeams = new BehaviorSubject<any[]>([]);
+  }
 
   setOpenLeague(newValue: string): void { this.$openLeague.next(newValue); }
   getOpenLeague(): Observable<string> { return this.$openLeague.asObservable(); }
+
+  setPlayers(newValue: any[]): void { this.$players.next(newValue); }
+  getPlayers(): Observable<any[]> { return this.$players.asObservable(); }
+
+  setAllTeams(newValue: any[]): void { this.$allTeams.next(newValue); }
+  getAllTeams(): Observable<any[]> { return this.$allTeams.asObservable(); }
 
 }
