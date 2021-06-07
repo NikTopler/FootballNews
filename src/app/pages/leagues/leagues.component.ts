7af29bcce0853ded6f74831b82744757dd9e8a40
Nik Topler <nik.topler21@gmail.com>
2021-06-07 09:53:40 +0200

    Setup league component

--- src/app/pages/leagues/leagues.component.ts
+++ src/app/pages/leagues/leagues.component.ts
@@ -9,12 +9,21 @@ import { LeagueService } from 'src/app/services/league/league.service';
 })
 export class LeaguesComponent {
 
+  urlPath: string = '';
+  openLeague: string = '';
+
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private leagueService: LeagueService) {
-    route.params.subscribe(params => leagueService.setOpenLeague(params.league));
+    router.events.subscribe(() => this.urlPath = router.url);
+    route.params.subscribe(params => { leagueService.setOpenLeague(params.league) });
+    leagueService.getOpenLeague().subscribe((league) => {
+      this.openLeague = league;
+      leagueService.openLeague = league;
+    });
     leagueService.fetchPlayers();
     leagueService.fetchTeams();
+    this.urlPath = router.url;
   }
 }
