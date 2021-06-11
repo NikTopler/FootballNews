16e697f5fb4a09f8359da681e137b0ae01ea1c27
Nik Topler <nik.topler21@gmail.com>
2021-06-12 00:56:28 +0200

    Add functionality to standings component

--- src/app/pages/leagues/standings/standings.component.ts
+++ src/app/pages/leagues/standings/standings.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommService } from 'src/app/services/comm/comm.service';
 import { LeagueService, StandingsInterface } from 'src/app/services/league/league.service';
 
 @Component({
@@ -6,14 +7,25 @@ import { LeagueService, StandingsInterface } from 'src/app/services/league/leagu
   templateUrl: './standings.component.html',
   styleUrls: ['./standings.component.scss']
 })
-export class StandingsComponent implements OnInit {
+export class StandingsComponent {
 
-  constructor() { }
   standingsArray: StandingsInterface[] = [];
+  selectOpen: boolean = false;
+  seasons: any[] = [{text: '2021/22', active: false}, {text: '2020/21', active: true}];
+  selectedSeason: string = '';
 
-  ngOnInit(): void {
+  constructor(
     private leagueService: LeagueService,
+    private comm: CommService) {
     leagueService.getStandings().subscribe(data => this.standingsArray = data);
+    this.setSelectedSeason();
   }
 
+  setSelectedSeason() { this.selectedSeason = this.seasons.filter(function(season) { return season.active})[0].text }
+  changeOption(text: string, id: string) {
+    this.comm.changeOption(text, id, this.seasons, this.selectOpen);
+    this.setSelectedSeason();
+  }
+  manageSelect(id: string) { this.comm.manageSelect(id, this.selectOpen) }
+  getPercentage(all: number, part: number) { return Math.floor((part/all)*100); }
 }
