30591bd2def72aac456313164f9f65ece2b2051e
Nik Topler <nik.topler21@gmail.com>
2021-09-10 15:02:19 +0200

    Update db url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  db: 'https://footballdb.gabercujes.com/v2',
+  db: 'https://api.sinea.live/football-news/v2',
   googleLoginProvider: '501636115719-6agkr3dcrhs7jpd0pl5dfuei7pqjbkg1.apps.googleusercontent.com',
   facebookLoginProvider: '294668175345203',
   amazonLoginProvider: 'amzn1.application-oa2-client.c556fb7f5e63425580ff821df5733a88',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  db: 'https://footballdb.gabercujes.com/v2',
+  db: 'https://api.sinea.live/football-news/v2',
   googleLoginProvider: '501636115719-6agkr3dcrhs7jpd0pl5dfuei7pqjbkg1.apps.googleusercontent.com',
   facebookLoginProvider: '294668175345203',
   amazonLoginProvider: 'amzn1.application-oa2-client.c556fb7f5e63425580ff821df5733a88',
